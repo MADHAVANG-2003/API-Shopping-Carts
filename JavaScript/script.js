6113e63e83220b197b09fc8ad8e1f30a4c06c85e
MADHAVANG-2003 <madhavangopi2003@gmail.com>
2025-01-13 20:44:55 +0530

    new update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,29 +1,18 @@
-const row = document.getElementById("cart-row");
-
-fetch('https://fakestoreapi.com/products/1')
-            .then(res=>res.json())
-            .then(json=>{
-              let title = json.title;
-              let detail = json.description;
-              let price = json.price;
-              let image = json.image;
-              row.innerHTML = `<div class="col">
-                                <div class="cart-box">
-                                <div class="cart-img">
-                                  <img src="${image}" alt="">
-                                </div>
-                                <h3 class="title">${title}</h3>
-                                <p class="detail">${detail}</p>
-                                <h5 class="price"><span>Price :</span>$${price}</h5>
-                                <Button class="btn btn-warning w-100">Add to Cart</Button>
-                              </div>
-                            </div> `
-            });
-
+const btn = document.querySelector(".btn")
 const row2 = document.getElementById("card-row2");
 
-fetch('https://fakestoreapi.com/products').then(res=>res.json()).then((output)=>{
-  const cards = output.map(card=>`<div class="col">
+
+async function load(){
+  const url = 'https://fakestoreapi.com/products';
+  const response = await fetch(url);
+  if(!response.ok){
+    throw new Error("Something went wrong");
+  }
+  row2.innerHTML = "please wait..."
+  const cards= await response.json();
+  row2.innerHTML =""
+  cards.forEach(card=>{
+    const result = `<div class="col">
                             <div class="cart-box">
                               <div class="cart-img d-flex align-items-center justify-content-center">
                                 <img src="${card.image}" alt="">
@@ -33,10 +22,29 @@ fetch('https://fakestoreapi.com/products').then(res=>res.json()).then((output)=>
                               <h5 class="price"><span>Price :</span>$${card.price}</h5>
                               <Button class="btn btn-warning w-100">Add to Cart</Button>
                             </div>
-                          </div>`).join('');
+                          </div>`
+                          row2.innerHTML+=result;
+  }).join('');
+  
+}
 
-                          row2.innerHTML = cards
+// fetch('https://fakestoreapi.com/products').then(res=>res.json()).then((output)=>{
+//   const cards = output.map(card=>`<div class="col">
+//                             <div class="cart-box">
+//                               <div class="cart-img d-flex align-items-center justify-content-center">
+//                                 <img src="${card.image}" alt="">
+//                               </div>
+//                               <h3 class="title">${card.title}</h3>
+//                               <p class="detail" style="text-align:empasis;">${card.description}</p>
+//                               <h5 class="price"><span>Price :</span>$${card.price}</h5>
+//                               <Button class="btn btn-warning w-100">Add to Cart</Button>
+//                             </div>
+//                           </div>`).join('');
+
+//                           row2.innerHTML = cards
                           
                           
-})
+// });
+
+btn.addEventListener("click",load)
             
